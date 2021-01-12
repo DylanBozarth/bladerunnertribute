f6dc00ac345a3b104be6c9c4476a78aac148ba2f
dylan <dylanabozarth@gmail.com>
2021-01-11 16:41:24 -0800

    music and title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
      <div className='background first container-fluid'>
-
+<h1 className="title">Blade Runner <br /> 2049</h1>
      </div>
      <div className='background second'>
       
