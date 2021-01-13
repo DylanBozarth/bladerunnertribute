1d9b70add931fea27d21cad3da795b32a2b99b83
dylan <dylanabozarth@gmail.com>
2021-01-12 21:09:12 -0800

    audio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  var audio = new Audio('theme.mp3');
-
-
+  var audio = new Audio("theme.mp3");
+  const Playit = () => {
+audio.play();
+  }
+  useEffect(() => Playit(), [Playit]);
 
   return (
     <div className="App">
-     <div className='background first container-fluid'>
-<h1 className="title text-center">Blade Runner <br /> 2049</h1>
-     </div>
-     <div className='background second'>
-      
-     </div>
-     <div className='background third'>
-       
-     </div>
+      <div className="background first container-fluid">
+        <h1 className="title text-center">
+          Blade Runner <br /> 2049
+        </h1>
+      </div>
+      <div className="background second"></div>
+      <div className="background third"></div>
     </div>
   );
 }
