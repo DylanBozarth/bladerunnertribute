bab91673cb6440449824286e9ec70455df93b0f0
dylan <dylanabozarth@gmail.com>
2021-01-13 10:00:40 -0800

    working audio button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [audio, SetAudio] = useState("");
+
   const Playit = () => {
-    var audio = new Audio("theme.mp3");
     audio.play();
-}
-useEffect(() => {Playit()}, []);
+  };
+  const Stopit = () => {
+    
+    audio.pause();
+  };
+  useEffect(() => {
+    SetAudio(new Audio("theme.mp3"));
+  }, []);
 
   return (
     <div className="App">
-      
-      <div className="background first container-fluid"><button className="button" onClick={Playit}>Play Audio</button>
+      <div className="background first container-fluid">
+        <button className="button" onClick={Playit}>
+          Play Audio
+        </button>{" "}
+        <br />
+        <button className="button2" onClick={Stopit}>
+          Stahp Audio
+        </button>
         <h1 className="title text-center">
           Blade Runner <br /> 2049
         </h1>
