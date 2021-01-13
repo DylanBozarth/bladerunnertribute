51b9d8e6e4aba5a3b18ea29a90f8d7864d3089b3
dylan <dylanabozarth@gmail.com>
2021-01-13 13:35:42 -0800

    scroll setup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [audio, SetAudio] = useState("");
+  const [audioState, SetAudio] = useState("");
 
   const Playit = () => {
-    audio.play();
+    audioState.play();
   };
   const Stopit = () => {
     
-    audio.pause();
+    audioState.pause();
   };
   useEffect(() => {
     SetAudio(new Audio("theme.mp3"));
@@ -17,19 +17,26 @@ function App() {
 
   return (
     <div className="App">
-      <div className="background first container-fluid">
-        <button className="button" onClick={Playit}>
+      <div className="container text-center dock"> <button className="button" onClick={Playit}>
           Play Audio
         </button>{" "}
         <br />
         <button className="button2" onClick={Stopit}>
           Stahp Audio
-        </button>
+        </button></div>
+      <div className="background first container-fluid">
+       
+       
         <h1 className="title text-center">
           Blade Runner <br /> 2049
         </h1>
+        
       </div>
-      <div className="background second"></div>
+      <div className="background second">
+        <div>aaaaa</div>
+        <div>aaaaa</div>
+        <div>aaaaa</div>
+      </div>
       <div className="background third"></div>
     </div>
   );
