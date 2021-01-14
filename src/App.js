c9c4fa9d3a79abb08c64ead2e3d4b028f6328a2f
dylan <dylanabozarth@gmail.com>
2021-01-14 14:12:38 -0800

    dock issues

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import {motion, useViewportScroll, useTransform} from 'framer-motion'
+import { motion, useViewportScroll, useTransform } from "framer-motion";
 
 function App() {
   const [audioState, SetAudio] = useState("");
-const { scrollYProgress } = useViewportScroll()
+  const { scrollYProgress } = useViewportScroll();
   const Playit = () => {
     audioState.play();
   };
   const Stopit = () => {
-    
     audioState.pause();
   };
   useEffect(() => {
@@ -18,36 +17,40 @@ const { scrollYProgress } = useViewportScroll()
   const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
   return (
     <div className="App">
-      <div className="container text-center dock"> <button className="button" onClick={Playit}>
+      <div className="container  dock">
+        {" "}
+        <button className="button" onClick={Playit}>
           Play Audio
         </button>{" "}
-        <br />
+     
         <button className="button2" onClick={Stopit}>
-          Stahp Audio
-        </button></div>
+          Pause Audio
+        </button>
+      </div>
       <div className="background first ">
-       
-       
         <h1 className="title text-center ">
           Blade Runner <br /> 2049
         </h1>
-       
       </div>
       <div className="background second">
-      <div className="box">“Dying for the right cause. It's the most human thing we can do.”</div> 
-    
-
+        
       </div>
-      <div className="background third">  </div>
-      <div className="background forth"><div className="box">“Dying for the right cause. It's the most human thing we can do.”</div>   </div>
-      <div className="background fifth">  </div>
-      <div className="background sixth">  <a href="">Buy the movie here</a></div>
+      <div className="background third"> </div>
+      <div className="background forth">
+        <div className="box">
+          “Dying for the right cause. It's the most human thing we can do.”
+        </div>{" "}
+      </div>
+      <div className="background fifth"> </div>
+      <div className="background sixth">
+        {" "}
+        <a href="" className="title">Buy the movie here</a>
+      </div>
     </div>
   );
 }
 
 export default App;
-
 
 //“Pain reminds you the joy you felt was real. More joy, then.”
 //“All the courage in the world cannot alter fact.”
